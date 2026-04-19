cfac8c28335743ca9e75158108d81edfda92a639
Avi Davidovits <avidavi1023@gmail.com>
2026-04-19 12:14:28 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+import pizza from './pizza.js';
+import{subSandwich} from './pizza.js'
+
+console.log('Hello from node!');
+
+pizza(24, 'cheese', 'frosting', 'chips');
+subSandwich(12, 'turkey', 'frosting', 'chips');
---
+++ pizza.js
@@ -0,0 +1,16 @@
+function pizza(size, ...toppings){
+    console.log(`Making a ${size} inch pizza with:`);
+    for (let topping of toppings){
+        console.log(topping);
+    }
+}
+
+function subSandwich(size, type, ...toppings){
+    console.log(`Making a ${size} inch ${type} sub sandwich with:`);
+    for (let topping of toppings){
+            console.log(topping);
+        }
+}
+
+export default pizza;
+export{subSandwich};
